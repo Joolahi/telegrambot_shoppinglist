c2f15f10376ffbd13364ecc213f38a3db819c78b
Joolahi <joonaslahti1998@gmail.com>
2023-02-06 19:26:31 +0200

    added new python file

--- ShoppinglistBot.py
+++ ShoppinglistBot.py
@@ -1,55 +1,54 @@
-from telegram.ext import Updater, CommandHandler
+
 import sqlite3
 import os
 from flask import Flask, request
+from telegram.bot import Bot
 
 TOKEN = '5945945191:AAEG4NkyYNSSmHXHO_mqTiQLZnqAopGnQDo'
+bot = Bot(toke=TOKEN)
 
-updater = Updater(token=TOKEN)
-dispatcher = updater.dispatcher
-
-def start(update, context):
-    update.message.reply_text('Shopping list, \n /add -> add item to shopping list \n /show -> will show you what is on the list')
+def start(chat_id):
+    bot.send_message(chat_id=chat_id,text='Shopping list, \n /add -> add item to shopping list \n /show -> will show you what is on the list')
 
 
-def adding(update, context):
-    args = context.args
+def adding(chat_id, items):
     database = sqlite3.connect('Shoppinglist.db')
     connect = database.cursor()
     connect.execute("CREATE TABLE IF NOT EXISTS shopping_list (id INTEGER PRIMARY KEY, item TEXT)")
-    for item in args:
+    for item in items:
         connect.execute("INSERT INTO shopping_list (item) VALUES (?)", (item,))
     database.commit()
     database.close()
-    update.message.reply_text("Item addded to shopping list!")
+    bot.send_message(chat_id=chat_id, text="Item addded to shopping list!")
 
 
 
 
-def showingList(update, context):
+def showingList(chat_id):
     database = sqlite3.connect('Shoppinglist.db')
     connect = database.cursor()
     connect.execute("SELECT item FROM shopping_list")
     items = [item[0] for item in connect.fetchall()]
     database.close
     if not items:
-        update.message.reply_text('Shopping list is empty')
+        bot.send_message(chat_id=chat_id, text="Shopping list is empty.")
     else:
-        update.message.reply_text('Shopping list: \n' + '\n'.join(items))
-
-DA = dispatcher.add_handler
-start_handler = CommandHandler('start',start)
-DA(start_handler)
-add_handler = CommandHandler('add',adding)
-DA(add_handler)
-list_handler = CommandHandler('show',showingList)
-DA(list_handler)
+        bot.send_message(chat_id=chat_id, text="Shopping list:\n\n" + "\n".join(items))
 
 server = Flask(__name__)
 
 def getMessage():
-    updater.dispatcher(request.get_json(force=True))
-    return '!', 200
+    update = request.get_json(force=True)
+    chat_id = update["message"]["chat"]["id"]
+    text = update["message"]["text"]
+    if text == "/start":
+        start(chat_id)
+    elif text.startswith("/add"):
+        items = text.split()[1:]
+        adding(chat_id, items)
+    elif text == "/list":
+        showingList(chat_id)
+    return "!", 200
 
 if __name__ == "__main__":
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
